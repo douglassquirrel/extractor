4958f5775286d4fe582c3eaff8149dd5456aab66
douglassquirrel <squirrel@blindpages.com>
2010-04-02 19:11:44 +0100

    events

---
+++ library/vancouver/events.rb
@@ -0,0 +1,7 @@
+@title = "Vancouver 2010 - Events"
+
+doc = fetch("http://www.vancouver2010.com/")
+nodes = doc.xpath("//ul[@id='nav']/li/ul/li").slice(1..-1)
+nodes.each { |node|
+  add_link(node.xpath("a/text()").to_s, "http://www.vancouver2010.com" + node.xpath("a/@href").to_s)
+}
