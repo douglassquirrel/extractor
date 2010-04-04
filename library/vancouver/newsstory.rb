dff83b4dfb72fa69262f5b0d50e2bfc81a6b3a43
douglassquirrel <squirrel@blindpages.com>
2010-04-04 15:35:28 +0100

    Fix ugly double xpath

--- library/vancouver/newsstory.rb
+++ library/vancouver/newsstory.rb
@@ -1,5 +1,5 @@
 doc = fetch("http://www.vancouver2010.com/olympic-news/n/news/" + params["title"])
 @title = doc.xpath("//h1/text()").to_s
-nodes = doc.xpath("//div[@class='contentBlock']")[0].xpath("p")
+nodes = doc.xpath("(//div[@class='contentBlock'])[1]/p")
 nodes.each { |node| @paragraphs << inner_xml_of(node) }
 add_link("Back to news", "/vancouver/news")
