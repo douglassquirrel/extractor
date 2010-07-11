3741ac41193fa954d7da8d5c5dfccd339914d1ab
douglassquirrel <squirrel@blindpages.com>
2010-07-11 22:31:04 +0100

    permissions for events and groups

--- library/facebook/home.rb
+++ library/facebook/home.rb
@@ -2,7 +2,10 @@ if !params["access_token"]
   @title = "Login needed"
   add_link("Please log in", "https://graph.facebook.com/oauth/authorize?client_id=131137803591593"\
                                                                     + "&redirect_uri=http://unlock.blindpages.com/facebook/login"\
-                                                                    + "&scope=user_activities,friends_activities,read_stream")
+                                                                    + "&scope=user_activities,friends_activities,"\
+                                                                           + "user_groups,friends_groups,"\
+                                                                           + "user_events,friends_events,"\
+                                                                           + "read_stream")
 else
   token = params["access_token"]
   profile = JSON.parse(open(URI.encode("https://graph.facebook.com/me?access_token=#{token}")).read)
