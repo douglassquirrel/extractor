381405c448a4c56aae072481c28c3fde03d48565
douglassquirrel <squirrel@blindpages.com>
2010-07-11 13:16:20 +0100

    Fix multiline problem

--- library/facebook/login.rb
+++ library/facebook/login.rb
@@ -3,9 +3,9 @@ if !params["code"]
   @paragraphs << "Logging in failed for some reason (access code not supplied by Facebook). Sorry I can't help further."
   add_link("You can try again if you want", "/facebook/home")
 else
-  token_url = URI.encode("https://graph.facebook.com/oauth/access_token?client_id=131137803591593"
-                                                                    + "&redirect_uri=http://unlock.blindpages.com/facebook/login"
-                                                                    + "&client_secret=5b67e7143dbc06dff39fbdbd52980797"
+  token_url = URI.encode("https://graph.facebook.com/oauth/access_token?client_id=131137803591593"\
+                                                                    + "&redirect_uri=http://unlock.blindpages.com/facebook/login"\
+                                                                    + "&client_secret=5b67e7143dbc06dff39fbdbd52980797"\
                                                                     + "&code=#{params['code']}")
   open(token_url).read =~ /^access_token=(.*)$/
   token = $1
