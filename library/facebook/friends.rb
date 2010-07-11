4f01e38537347683534f7c725d75df01b6e4b324
douglassquirrel <squirrel@blindpages.com>
2010-07-11 13:38:33 +0100

    Get json for friends

--- library/facebook/friends.rb
+++ library/facebook/friends.rb
@@ -4,5 +4,6 @@ if !params["access_token"]
 else
   token = params["access_token"]
   @title = "Friends"
-  @paragraphs << "Token is #{token}"
+  friends_json = open(URI.encode("https://graph.facebook.com/me?access_token=#{token}")).read
+  @paragraphs << friends_json
 end
