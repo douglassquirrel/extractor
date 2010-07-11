c1fda10415173e0d99dee470ff3c36e4d42bf4ae
douglassquirrel <squirrel@blindpages.com>
2010-07-11 16:22:10 +0100

    Request friend activities

--- library/facebook/home.rb
+++ library/facebook/home.rb
@@ -1,6 +1,8 @@
 if !params["access_token"] 
   @title = "Login needed"
-  add_link("Please log in", "https://graph.facebook.com/oauth/authorize?client_id=131137803591593&redirect_uri=http://unlock.blindpages.com/facebook/login")
+  add_link("Please log in", "https://graph.facebook.com/oauth/authorize?client_id=131137803591593"\
+                                                                    + "&redirect_uri=http://unlock.blindpages.com/facebook/login"\
+                                                                    + "&scope=friends_activities")
 else
   token = params["access_token"]
   add_link("Friends", URI.encode("/facebook/friends?access_token=#{token}"))
