4fe626f5fe22ef7b99eb2dd46c0786e5f4ec07b6
douglassquirrel <squirrel@blindpages.com>
2010-07-11 13:34:49 +0100

    add friends

--- library/facebook/friends.rb
+++ library/facebook/friends.rb
@@ -3,5 +3,6 @@ if !params["access_token"]
   add_link("Please log in", "https://graph.facebook.com/oauth/authorize?client_id=131137803591593&redirect_uri=http://unlock.blindpages.com/facebook/login")
 else
   token = params["access_token"]
-  add_link("Friends", URI.encode("/facebook/friends?access_token=#{token}"))
+  @title = "Friends"
+  @paragraphs << "Token is #{token}"
 end
