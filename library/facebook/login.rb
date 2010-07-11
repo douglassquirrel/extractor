e1c29b25c924ce60746e6a382628879302262e1c
douglassquirrel <squirrel@blindpages.com>
2010-07-11 13:03:40 +0100

    Get token for facebook

--- library/facebook/login.rb
+++ library/facebook/login.rb
@@ -3,8 +3,9 @@ if !params["code"]
   @paragraphs << "Logging in failed for some reason (access code not supplied by Facebook). Sorry I can't help further."
   add_link("You can try again if you want", "/facebook/home")
 else
-  @title = "Got code"
-  @paragraphs << "Code is #{params['code']}"
+  token_response = open("https://graph.facebook.com/oauth/access_token?client_id=131137803591593&redirect_uri=http://unlock.blindpages.com/facebook/login&client_secret=5b67e7143dbc06dff39fbdbd52980797&code=#{params['code']}").read
+  @paragraphs << "Token response was:"
+  @paragraphs << token_response
 end
 
 
