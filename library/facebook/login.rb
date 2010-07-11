64a12d0a5ae7eedad86c7a393474407a6eb1305f
douglassquirrel <squirrel@blindpages.com>
2010-07-11 13:07:18 +0100

    Fix problem with uri encoding

--- library/facebook/login.rb
+++ library/facebook/login.rb
@@ -3,7 +3,8 @@ if !params["code"]
   @paragraphs << "Logging in failed for some reason (access code not supplied by Facebook). Sorry I can't help further."
   add_link("You can try again if you want", "/facebook/home")
 else
-  token_response = open("https://graph.facebook.com/oauth/access_token?client_id=131137803591593&redirect_uri=http://unlock.blindpages.com/facebook/login&client_secret=5b67e7143dbc06dff39fbdbd52980797&code=#{params['code']}").read
+  token_url = URI.encode("https://graph.facebook.com/oauth/access_token?client_id=131137803591593&redirect_uri=http://unlock.blindpages.com/facebook/login&client_secret=5b67e7143dbc06dff39fbdbd52980797&code=#{params['code']}")
+  token_response = open(token_url).read
   @paragraphs << "Token response was:"
   @paragraphs << token_response
 end
