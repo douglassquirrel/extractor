8545c333bc2626f6e214187144118953b4d17950
douglassquirrel <squirrel@blindpages.com>
2010-07-11 20:21:11 +0100

    news page

---
+++ library/facebook/news.rb
@@ -0,0 +1,9 @@
+if !params["access_token"] 
+  @title = "Login needed"
+  add_link("Please log in", "https://graph.facebook.com/oauth/authorize?client_id=131137803591593&redirect_uri=http://unlock.blindpages.com/facebook/login")
+else
+  token = params["access_token"]
+  @title = "News"
+  news = JSON.parse(open(URI.encode("https://graph.facebook.com/id=#{params['id']}/feed?access_token=#{token}")).read)["data"]
+  @paragraphs << news.inspect
+end
