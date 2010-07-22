530c8048b29106203ef6ecb8776745d56c42c4a2
douglassquirrel <squirrel@blindpages.com>
2010-07-22 07:00:58 +0100

    Hide fields again

--- library/facebook/news.rb
+++ library/facebook/news.rb
@@ -8,8 +8,8 @@ else
   @paragraphs << "</p><form action='/facebook/news' method='get'><p>" \
                                                   + "<label for='message'>Write a message:</label>" \
                                                   + "<input type='text' id='message' name='message'/>" \
-                                                  + "<input type='text' id='id' name='id' value='#{params['id']}'/>" \
-                                                  + "<input type='text' id='access_token' name='access_token' value='" + URI.encode(token) + "'/>" \
+                                                  + "<input type='hidden' id='id' name='id' value='#{params['id']}'/>" \
+                                                  + "<input type='hidden' id='access_token' name='access_token' value='" + URI.encode(token) + "'/>" \
                                                   + "<input type='submit'/></p></form><p>"
   if params["message"]
     @paragraphs << "Posted message #{params['message']}"
